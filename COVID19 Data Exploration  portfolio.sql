2de0d96d4913f31ce0f33978fa7663c97ef7d185
markchinwike <160749574+markchinwike@users.noreply.github.com>
2024-03-12 22:41:41 +0100

    Update COVID19 Data Exploration  portfolio.sql

--- COVID19 Data Exploration  portfolio.sql
+++ COVID19 Data Exploration  portfolio.sql
@@ -172,3 +172,5 @@ Join PortfolioWareHouse..CovidVaccinations$ vac
 	and dea.date = vac.date
 where dea.continent is not null 
 
+Conclusion
+The exploration of COVID-19 data has provided valuable insights into the global impact of the pandemic. While the crisis may have abated, the lessons learned from analyzing infection rates, death counts, and vaccination progress remain invaluable. This data serves as a reminder of the importance of preparedness and resilience in the face of future health crises. By understanding the patterns and dynamics of COVID-19, we can better equip ourselves to respond effectively to future challenges and safeguard public health worldwide.
